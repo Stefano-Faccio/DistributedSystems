9a47212d039e687e77a139041f150fe702e42cc8
Stefano <stefano.faccio@studenti.unitn.it>
2023-05-18 16:58:08 +0200

    C# project

---
+++ DistributedKeyValueStore.NET/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace DistributedKeyValueStore.NET
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
