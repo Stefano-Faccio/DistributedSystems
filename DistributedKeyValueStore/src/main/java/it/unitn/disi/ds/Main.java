080e530ef91331cbf9ee637f680eb01a82a07610
Stefano <stefano.faccio@studenti.unitn.it>
2023-05-18 16:54:35 +0200

    Java project (speriamo di non usarlo)

---
+++ DistributedKeyValueStore/src/main/java/it/unitn/disi/ds/Main.java
@@ -0,0 +1,9 @@
+package it.unitn.disi.ds;
+
+import akka.actor.typed.ActorSystem;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
