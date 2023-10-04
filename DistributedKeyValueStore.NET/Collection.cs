e3a7a019ef45c3c708199a0ebb90f6fdcdb03772
Stefano <stefano.faccio@studenti.unitn.it>
2023-10-04 19:53:37 +0200

    Aggiunta classi base

---
+++ DistributedKeyValueStore.NET/Collection.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DistributedKeyValueStore.NET
+{
+    //https://stackoverflow.com/questions/1273139/c-sharp-java-hashmap-equivalent
+    internal class Collection 
+    {
+        private Dictionary<uint, Document> dictionary;
+
+        public Collection()
+        {
+            dictionary = new Dictionary<uint, Document>();
+        }
+
+        public void Add(uint key, string value, uint version) 
+        {
+            Document document = new Document(key, value, version);
+        }
+
+        public void Add(uint key, string value)
+        {
+            Add(key, value, 0);
+        }
+
+        public Document Remove(uint key)
+        {
+            Document tmp = this[key];
+
+            dictionary.Remove(key);
+
+            return tmp;
+        }
+
+        //Overload di []
+        public Document this[uint key]
+        {
+            get => dictionary[key];
+            //set => SetValue(key, value);
+        }
+    }
+}
---
+++ DistributedKeyValueStore.NET/Document.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DistributedKeyValueStore.NET
+{
+    internal class Document
+    {
+        private readonly uint key;
+        private uint version;
+        private string value;
+        private bool preWriteBlock;
+        public uint Key => key;
+        public string Value => value;
+        public uint Version => version;
+
+        public Document(uint key, string value)
+        {
+            this.key = key;
+            this.value = value;
+            this.version = 0;
+            this.preWriteBlock = false;
+        }
+
+        public Document(uint key, string value, uint version) : this(key, value)
+        {
+            this.version = version;
+        }
+
+        public void ClearPreWriteBlock() { this.preWriteBlock = false; }
+
+        public void SetPreWriteBlock() { this.preWriteBlock = true; }
+
+        public void Update(string value, uint version)
+        {
+            if (version <= this.version)
+                throw new Exception("It is not possible to update a value with a less recent one");
+
+            this.value = value;
+            this.version = version;
+
+            //Pulisco il pre-write block poichè la write è avvenuta con successo
+            this.ClearPreWriteBlock();
+        }
+
+        public override string? ToString()
+        {
+            return $"[Key:{this.key}, Value:{this.Value}, Version:{this.version}, PreWriteBlock:{this.preWriteBlock}]";
+        }
+    }
+}
