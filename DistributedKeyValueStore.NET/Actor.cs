faed876e9d91b15dd6f6dc9525343f34bafad949
Stefano <stefano.faccio@studenti.unitn.it>
2023-10-05 09:42:30 +0200

    Aggiunta Akka.NET

---
+++ DistributedKeyValueStore.NET/Actor.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Akka.Actor;
+
+namespace DistributedKeyValueStore.NET
+{
+    internal class GreetingActor : ReceiveActor
+    {
+        public GreetingActor()
+        {
+            // Tell the actor to respond to the Greet message
+            Receive<Greet>(greet => Console.WriteLine($"Hello {greet.Who}"));
+        }
+        protected override void PreStart()
+        {
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("Good Morning, we are awake!");
+        }
+
+        protected override void PostStop()
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("Good Night, going to bed!");
+        }
+    }
+}
